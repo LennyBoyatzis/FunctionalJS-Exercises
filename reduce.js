25956190a35fa7b3f9c7fa8e8a54dc860e806b3d
Lenny Boyatzis <lennyboyatzis@gmail.com>
2016-03-20 15:56:55 +1100

    Understanding reduce

---
+++ reduce.js
@@ -0,0 +1,25 @@
+const VOTES = [
+  "angular",
+  "angular",
+  "react",
+  "react",
+  "react",
+  "angular",
+  "ember",
+  "react",
+  "vanilla"
+]
+
+const initialValue = {};
+
+const reducer = (tally, vote) => {
+  if (!tally[vote]) {
+    tally[vote] = 1;
+  } else {
+    tally[vote] = tally[vote] + 1;
+  }
+
+  return tally;
+}
+
+const result = votes.reduce(reducer, initialValue)
