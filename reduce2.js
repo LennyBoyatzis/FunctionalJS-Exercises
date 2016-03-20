a2df973c3e10121f6b0a65521b2f09c3d97749b4
Lenny Boyatzis <lennyboyatzis@gmail.com>
2016-03-20 16:26:06 +1100

    common mistakes

---
+++ reduce2.js
@@ -0,0 +1,27 @@
+const reducer(accumulator, value, index, array) {
+  let intermediaryValue = accumulator + value
+  if (index === array.length - 1) {
+    return intermediaryValue / array.length
+  }
+
+  return intermediaryValue
+}
+
+const DATA = [1,2,3,4,5,6,1]
+const mean = data.reduce(reducer, 0)
+
+
+const DATA = ["vote1", "vote2", "vote1", "vote2"]
+
+const reducer = (accumulator, value) => {
+  if (accumulator[value]) {
+    accumulator[value] = accumulator[value] + 1;
+  } else {
+    accumulator[value] = 1;
+  }
+
+  return accumulator;
+}
+
+// What out for this kind of bug!!!! Make sure you always pass in an initial value otherwise it defaults to the first item in the array
+const tally = data.reduce(reducer, {})
