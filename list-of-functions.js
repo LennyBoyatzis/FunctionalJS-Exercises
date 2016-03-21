a44ce16f464ad4a1490888c8cea5233d2f62c9d0
Lenny Boyatzis <lennyboyatzis@gmail.com>
2016-03-22 09:45:50 +1100

    list of common functions

---
+++ list-of-functions.js
@@ -0,0 +1,37 @@
+head() - get the first element
+tail() - get all but the first element
+last() - get last element
+length() - count of elements
+
+
+// Predicates / Comparators
+----------------------------
+
+equal()
+greaterThan()
+lessThan()
+
+// List transformations
+----------------------------
+
+map()
+reverse()
+
+// List searches
+----------------------------
+
+find()
+filter()
+
+// Reducers / folds
+----------------------------
+
+reduce()
+any() - true if any values match predicate
+all() - true if all values match predicate
+sum()
+product()
+maximum()
+minimum()
+concat() - takes a list and returns a single concatenated list in list order
+merge() - takes a list of lists and returns a single, merged list in element order
