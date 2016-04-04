8afb31221615990d0aa4e8aa83c52fc7e3ccd8d6
Lenny Boyatzis <lennyboyatzis@gmail.com>
2016-04-05 09:27:35 +1000

    JSON.parse API

---
+++ JSONdotparse.js
@@ -0,0 +1,22 @@
+// The JSON.parse API
+//JSON.parse - method which parses a string as JSON
+
+// it takes two parameters
+// 1. The json string to be parsed
+// 2. A reviver (generally a function which is used to instruct JSON.parse how to parse certain values)
+
+var input = '[{"id":1,"title":"Gone with the Wind","publishDate":"1936-06-10T00:00:00.000Z","related":[80,3]},{"id":2,"title":"Freelancer","publishDate":"2015-08-11T00:00:00.000Z","related":[45,89]},{"id":3,"title":"A Christmas Carol","publishDate":"1843-12-19T00:00:00.000Z","related":[20,33]},{"id":4,"title":"The Cat in the Hat","publishDate":"1957-03-12T00:00:00.000Z","related":[50,10]}]'
+
+var reviver = (key, value) => {
+    if (key === '') return value
+    if (key === 'publishDate') {
+        return new Date(value)
+    }
+    return value
+}
+
+const resultWithOutReviver = JSON.parse(input)
+const resultWithReviver = JSON.parse(input, reviver)
+
+console.log('resultWithOutReviver -->', resultWithOutReviver)
+console.log('resultWithReviver -->', resultWithReviver)
