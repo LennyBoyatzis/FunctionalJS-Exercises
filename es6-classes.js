7e39a50f183b18472e07bb2e308da1911ed54de8
Lenny Boyatzis <lennyboyatzis@gmail.com>
2016-04-19 20:37:22 +1000

    understanding es6 classes

---
+++ es6-classes.js
@@ -0,0 +1,28 @@
+// es6 classes
+// -----------
+
+class Point {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+    toString() {
+        return '(' + this.x + ', ' + this.y + ')';
+    }
+}
+
+class ColorPoint extends Point {
+    constructor(x,y,color) {
+        super(x,y);
+        this.color = color;
+    }
+    toString() {
+        return super.toString() + ' in ' + this.color;
+    }
+}
+
+let cp = new ColorPoint(25, 8, 'green');
+cp.toString();
+
+console.log(cp instanceof ColorPoint); // true
+console.log(cp instanceof Point); // true
